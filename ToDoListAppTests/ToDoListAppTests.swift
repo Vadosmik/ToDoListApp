092c81f1eddbdb844f28117c1969abd33381c60f
Vadosmik <liontv05@gmail.com>
2024-12-26 12:53:51 +0100

    Initial Commit

---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by Vadzim Mikanovich on 26/12/2024.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
